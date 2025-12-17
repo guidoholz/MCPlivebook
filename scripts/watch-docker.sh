292f27c863e33418d46be14334cddfe043a381f9
Guido Holz / Supplydea <guido.holz@supplydea.com>
2025-12-17 10:04:17 +0100

    removed odd code

--- scripts/watch-docker.sh
+++ scripts/watch-docker.sh
@@ -2,10 +2,4 @@
 # Watch a livebook file for changes and sync to Livebook in Docker
 # Usage: ./watch-docker.sh <file_path>
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <file_path_in_container>"
-  echo "Example: $0 /data/notebooks/bjoern.livemd"
-  exit 1
-fi
-
 exec docker exec -i livebook livebook_tools watch "$1"
